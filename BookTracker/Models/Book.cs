1e593f30f2c7b9502e1a694968289466add8998f
AndrzejOsowski <andrzejosowski93@gmail.com>
2025-02-23 11:58:47 +0100

    Added base data models

---
+++ BookTracker/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace BookTracker.Models;
+
+public class Book (string name, string author)
+{
+    public int Id { get; set; }
+    public float Rating { get; set; }
+    public DateOnly StartingDate { get; set; }
+    public DateOnly FinishingDate { get; set; }
+    public bool Read { get; set; }
+    public List<Note> Notes { get; set; } = new();
+    
+    public string Name { get; set; } = name;
+    public string Author { get; set; } = author;
+}
---
+++ BookTracker/Models/Note.cs
@@ -0,0 +1,9 @@
+﻿namespace BookTracker.Models;
+
+public class Note(string title, string content)
+{
+    public int Id { get; set; }
+    
+    public string Title { get; set; } = title;
+    public string Content { get; set; } = content;
+}
